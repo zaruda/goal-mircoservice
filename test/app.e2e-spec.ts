711656ecd9162dd01e67b949f9dfdf560531f1f1
Ilya Zarudny <zaruda46@gmail.com>
2022-04-24 00:07:52 +0300

    Add procfile

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { GoalModule } from './../src/goal.module';
 
-describe('AppController (e2e)', () => {
+describe('Goal module (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [GoalModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
